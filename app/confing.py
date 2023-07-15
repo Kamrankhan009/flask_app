dc0f14573ee11e8f051cdbc00d10434f7729a9b3
unknown <zeeshan1148@gmail.com>
2023-07-15 10:13:53 +0500

    new updates

--- app/confing.py
+++ app/confing.py
@@ -4,7 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = "asdsdasdasdasdasdaas" #os.environ.get("FLASK_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://GWYGL:KingofKuwait22@GWYGL.mysql.pythonanywhere-services.com/GWYGL$shopdb'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+
+
 
--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,6 +1,20 @@
 const messageElement = document.getElementById('chat-message');
-const message = "The answer was within her reach. It was hidden in a box and now that box sat directly in front of her. She'd spent years searching for it and could hardly believe she'd finally managed to find it. She turned the key to unlock the box and then gently lifted the top. She held her breath in anticipation of finally knowing the answer she had spent so much of her time in search of. As the lid came off she could see that the box was empty.";
+// const arabicText = `مرحبًا بكم في موقع الويب الخاص بسيرفر "FORTPLAY"، حيث يمكنكم شراء المنتجات الخاصة بالسيرفر هنا. يمكنكم أيضًا من خلال الموقع رؤية ترتيبكم في البطولة، وأيضًا معرفة أكثر 10 أشخاص جالسين في غرف السيرفر ومدة وجودهم فيها. نحن نوظف المحترفين لصالح المتجر، حيث يمكنهم الحصول على وظيفة معنا. يحتوي الموقع على صفحة لإرسال الدعم للسيرفر، ومن خلاله يمكنكم دعم السيرفر بمبلغ مادي، وسيكرمكم سيرفرنا على الديسكورد برتبة تليق بكم. <span style="color: #936EC4;">يرجى ملاحظة أن هذا السيرفر تم تصميمه لنشر السعادة بينكم.</span>`;
 
+const arabicText = `مرحبًا بكم في موقع الويب الخاص بسيرفر "<span style="color: #FFD700;">FORTPLAY</span>"، حيث يمكنكم شراء المنتجات الخاصة بالسيرفر هنا. يمكنكم أيضًا من خلال الموقع رؤية ترتيبكم في البطولة، وأيضًا معرفة أكثر 10 أشخاص جالسين في غرف السيرفر ومدة وجودهم فيها. نحن نوظف المحترفين لصالح المتجر، حيث يمكنهم الحصول على وظيفة معنا. يحتوي الموقع على صفحة لإرسال الدعم للسيرفر، ومن خلاله يمكنكم دعم السيرفر بمبلغ مادي، وسيكرمكم سيرفرنا على الديسكورد برتبة تليق بكم. <br><span style="color: #936EC4;">يرجى ملاحظة أن هذا السيرفر تم تصميمه لنشر السعادة بينكم.</span>`;
+
+const englishText = `Welcome to the "<span style="color: #FFD700;">FORTPLAY</span>" server website, here you can purchase server related products. Through the site, you can also see your ranking in the tournament, as well as checking the top 10 people with the most time in the server rooms and the duration of their presence in them. We also offer jobs at the website, as we hire professionals for the shop. You can also send support to our server through the support page in the website, and through it you can support the server with a sum of money, and our server will honor you on Discord with the supporter badge of you.
+
+Additionally, we offer tournaments throughout the month with substantial prize pools to support professional players. Even if you are not a professional player in the game, there are events in which you can participate and win cash prizes.
+
+We seek to create a suitable place for Fortnite players, where they can communicate with each other on the "<span style="color: #FFD700;">FORTPLAY</span>" discord server and offer tournaments to encourage professional players, and we also offer events to make the server fun for everyone. We also provide business opportunities for those who seek opening a clan in our server, where they can get financial income.
+
+Please note that this server is created to spread happiness among players.`;
+
+
+// const englishText ="";
+
+const message = arabicText + "<br><br>" + englishText;
 function displayMessage(messageElement, message) {
   let index = 0;
   let displayText = "";
